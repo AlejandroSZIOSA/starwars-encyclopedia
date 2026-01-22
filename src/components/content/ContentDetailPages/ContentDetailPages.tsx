dcc064988e41c3fcc04fc6c8aa14b6af5f5e084a
AlejandroSZIOSA <ggato33@gmail.com>
2026-01-22 21:18:50 +0100

    created new components

---
+++ src/components/content/ContentDetailPages/ContentDetailPages.tsx
@@ -0,0 +1,7 @@
+import { type FC } from "react";
+
+const ContentDetailPages: FC = () => {
+  return <div>Content Detail Pages</div>;
+};
+
+export default ContentDetailPages;
---
+++ src/components/content/ContentLinksDetailPages/ContentLinksDetailPages.tsx
@@ -0,0 +1,7 @@
+import { type FC } from "react";
+
+const ContentLinksDetailPages: FC = () => {
+  return <div>Content Links Detail Pages</div>;
+};
+
+export default ContentLinksDetailPages;
