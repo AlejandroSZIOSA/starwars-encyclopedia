fbefc76b822bae2b31403e0e1e41a87d91edd113
AlejandroSZIOSA <ggato33@gmail.com>
2026-02-27 16:01:46 +0100

    importing multiple import as ones, cleanning the code

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,40 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 //Importing multiple imports from the same file using a namespace import (PAGE) to organize them into categories (HOME_PAGES and DETAILS_PAGES). This way, we can easily access the different page components based on their type (home or details) and keep our imports clean and organized.
-import * as PAGE from "./pages/index";
+import * as PAGES from "./pages/index";
+
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       /* element: <RootLayout />, */
       children: [
-        { path: "/", index: true, element: <PAGE.HOME_PAGES.FilmsHomePage /> },
-        { path: "/film/:id", element: <PAGE.DETAILS_PAGES.DetailsFilmPage /> },
-        { path: "/people", element: <PAGE.HOME_PAGES.PeopleHomePage /> },
+        { path: "/", index: true, element: <PAGES.HOME_PAGES.FilmsHomePage /> },
+        { path: "/film/:id", element: <PAGES.DETAILS_PAGES.DetailsFilmPage /> },
+        { path: "/people", element: <PAGES.HOME_PAGES.PeopleHomePage /> },
         {
           path: "/character/:id",
-          element: <PAGE.DETAILS_PAGES.CharacterDetailsPage />,
+          element: <PAGES.DETAILS_PAGES.CharacterDetailsPage />,
         },
-        { path: "/planets", element: <PAGE.HOME_PAGES.PlanetsHomePage /> },
+        { path: "/planets", element: <PAGES.HOME_PAGES.PlanetsHomePage /> },
         {
           path: "/planet/:id",
-          element: <PAGE.DETAILS_PAGES.PlanetDetailsPage />,
+          element: <PAGES.DETAILS_PAGES.PlanetDetailsPage />,
         },
-        { path: "/species", element: <PAGE.HOME_PAGES.SpeciesHomePage /> },
+        { path: "/species", element: <PAGES.HOME_PAGES.SpeciesHomePage /> },
         {
           path: "/specie/:id",
-          element: <PAGE.DETAILS_PAGES.SpecieDetailsPage />,
+          element: <PAGES.DETAILS_PAGES.SpecieDetailsPage />,
         },
-        { path: "/starships", element: <PAGE.HOME_PAGES.StarshipsHomePage /> },
+        { path: "/starships", element: <PAGES.HOME_PAGES.StarshipsHomePage /> },
         {
           path: "/starship/:id",
-          element: <PAGE.DETAILS_PAGES.StarshipDetailsPage />,
+          element: <PAGES.DETAILS_PAGES.StarshipDetailsPage />,
         },
-        { path: "/vehicles", element: <PAGE.HOME_PAGES.VehiclesHomePage /> },
+        { path: "/vehicles", element: <PAGES.HOME_PAGES.VehiclesHomePage /> },
         {
           path: "/vehicle/:id",
-          element: <PAGE.DETAILS_PAGES.VehicleDetailsPage />,
+          element: <PAGES.DETAILS_PAGES.VehicleDetailsPage />,
         },
       ],
     },
